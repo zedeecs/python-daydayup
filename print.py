70f6c2dc6a4ed2e3ff4f79873cf7d03cd4ded3ac
zedeecs <669338500@qq.com>
2020-03-22 22:36:11 +0800

    P10 变量 字符串 输入 大小写

--- print.py
+++ print.py
@@ -1,6 +1,16 @@
-name = input('What\'s your name? \n' ) # 变量 name,\n 换行
-print('Hello !' , name.upper() ) #调用变量 name
-print('Hello !' , name.lower() )
-print('Hello !' , name.capitalize() )
-print( name.count('b'))
-# print('jess')
+# P9 字符串
+# name = input('What\'s your name? \n' ) # 变量 name,\n 换行
+# print('Hello !' , name.upper() ) #调用变量 name
+# print('Hello !' , name.lower() )
+# print('Hello !' , name.capitalize() )
+# print( name.count('b'))
+# # print('jess')
+
+# P10 字符串
+# first_name = ('Pleace input your first name: ')
+# last_name = ('Pleace input your last name: ')
+# print('Hello, ' + first_name.upper() + last_name.capitalize())
+
+first_name = input('Pleace input your first name: ')
+last_name = input('Pleace input your last name: ')
+print('Hello, ' + first_name.upper() + ' ' + last_name.capitalize())
