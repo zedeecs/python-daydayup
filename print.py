02116243aadfb882051d1d8d6bfb4a0c3eaf59e5
zedeecs <669338500@qq.com>
2020-03-22 22:19:48 +0800

    P9 字符串

--- print.py
+++ print.py
@@ -1,1 +1,6 @@
-print('jess')
+name = input('What\'s your name? \n' ) # 变量 name,\n 换行
+print('Hello !' , name.upper() ) #调用变量 name
+print('Hello !' , name.lower() )
+print('Hello !' , name.capitalize() )
+print( name.count('b'))
+# print('jess')
